4272cfbe98d68a786174b84e3880166ceab1a5c6
olamrabti <oussamalamrabtii@gmail.com>
2024-05-28 13:31:34 +0100

    created files

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olamrabt <olamrabt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 12:14:49 by olamrabt          #+#    #+#             */
+/*   Updated: 2024/05/28 13:27:05 by olamrabt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h> // gettimeofday
+
+#endif
